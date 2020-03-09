bcc4c3c41e8c4278a4b249f8d3f3deec8d7045dc
mmgertrude <22059601+mmgertrude@users.noreply.github.com>
2020-03-09 09:58:29 +0100

    working?

--- ContentViewerApp/Startup.cs
+++ ContentViewerApp/Startup.cs
@@ -55,7 +55,7 @@ namespace ContentViewerApp
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Page}/{action=Index}/{id?}");
             });
         }
     }
